f4d04c21a69f44a78f294668733b348ac94cef81
championway <cpwearth.eed03@g2.nctu.edu.tw>
2019-01-30 18:03:08 +0800

    save my data

--- catkin_ws/src/mapping/src/pub_tf.py
+++ catkin_ws/src/mapping/src/pub_tf.py
@@ -9,17 +9,24 @@ def call_back(msg):
 	# Global Frame: "/map"
 	map_id = msg.header.frame_id
 	br = tf.TransformBroadcaster()
-	# map <---> gazebo coordinate
-	#br.sendTransform((0.0, 0.0, 0.0),tf.transformations.quaternion_from_euler(0, 0, -np.pi/2.0), rospy.Time.now() ,map_id , "/X1/base_footprint")
-	# robot <---> map
-	quat = (msg.pose.pose.orientation.x,\
+	position = [msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.position.z]
+	quat = [msg.pose.pose.orientation.x,\
 			msg.pose.pose.orientation.y,\
 			msg.pose.pose.orientation.z,\
-			msg.pose.pose.orientation.w)
+			msg.pose.pose.orientation.w]
 	r, p, y = tf.transformations.euler_from_quaternion(quat)
-	quat = tf.transformations.quaternion_from_euler (-r, -p, -y)
-	br.sendTransform((-msg.pose.pose.position.x, -msg.pose.pose.position.y, -msg.pose.pose.position.z), \
-					 (quat[0], quat[1], quat[2], quat[3]), rospy.Time.now(), map_id, "/X1/base_footprint")
+	tfros = tf.TransformerROS()
+	M = tfros.fromTranslationRotation(position, quat)
+	M_inv =  np.linalg.inv(M)
+	trans = tf.transformations.translation_from_matrix(M_inv)
+	rot = tf.transformations.quaternion_from_matrix(M_inv)
+	t = rospy.Time.from_sec(0)
+	#t = rospy.Time.now()
+	br.sendTransform(trans, rot, t, map_id, "/X1/base_footprint")
+
+	#quat = tf.transformations.quaternion_from_euler (-r, -p, -y)
+	br.sendTransform((0, 0, 0), \
+					 (0, 0, 0, 1), rospy.Time.now(), "map", "X1/odom")
 
 if __name__=="__main__":
 	# Tell ROS that we're making a new node.
