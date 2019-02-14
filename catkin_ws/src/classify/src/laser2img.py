c4249beba033ec5664c4d7a09bf0d36ef44bd482
championway <cpwearth.eed03@g2.nctu.edu.tw>
2019-02-14 15:36:45 +0800

    laser 2 img

--- catkin_ws/src/classify/src/laser2img.py
+++ catkin_ws/src/classify/src/laser2img.py
@@ -18,12 +18,13 @@ class laser2img():
 	def __init__(self):
 		self.node_name = rospy.get_name()
 		rospy.loginfo("[%s] Initializing " %(self.node_name))
-		rospy.Subscriber('/scan', LaserScan, self.call_back, queue_size = 1, buff_size = 2**24)
+		rospy.Subscriber('/X1/scan', LaserScan, self.call_back, queue_size = 1, buff_size = 2**24)
 
 		self.bin = 360
 		self.range_max = 5.5
-		self.border = 10
-		self.scale = 50.
+		self.border = 5
+		self.scale = 10.
+		self.point_size = 2
 
 	def init_param(self):
 		self.width = int(self.range_max*self.scale*2 + self.border*2)
@@ -37,12 +38,20 @@ class laser2img():
 
 		for i in range(self.bin):
 			if msg.ranges[i] != float("inf"):
-				rad = (i/360.)*2.*math.pi
+				rad = (i/360.)*2.*math.pi/2.
 				x = self.scale*msg.ranges[i] * np.cos(rad)
 				y = self.scale*msg.ranges[i] * np.sin(rad)
-				self.img[int(x + self.width/2.)][int(y + self.height/2.)] = 255
-		cv2.imwrite( "Image.jpg", self.img )
-		#print('Save image')
+				x_ = int(x + self.width/2.)
+				y_ = int(y + self.height/2.)
+				self.img[x_][y_] = 255
+				self.img = self.img_dilate(self.img, x_, y_)
+		cv2.imwrite( "Image.jpg", self.img)
+
+	def img_dilate(self, img, x, y):
+		for m in range(-self.point_size, self.point_size + 1):
+			for n in range(-self.point_size, self.point_size + 1):
+				img[x + m][y + n] = 255
+		return img
 
 if __name__ == '__main__':
 	rospy.init_node('laser2img')
