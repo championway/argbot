8b25210576ac7b756c0f62f7c5fa944a58a998cc
championway <cpwearth.eed03@g2.nctu.edu.tw>
2019-03-15 11:46:24 +0800

    pcl roi

--- catkin_ws/src/rgbd_camera/src/image_process.cpp
+++ catkin_ws/src/rgbd_camera/src/image_process.cpp
@@ -48,6 +48,7 @@ Publish:
 
 //OpenCV lib
 #include <opencv/cv.h>
+#include <opencv2/opencv.hpp>
 
 #define PI 3.14159
 using namespace std;
@@ -70,6 +71,7 @@ private:
 	sensor_msgs::ImagePtr img_msg;
 	cv_bridge::CvImage cvIMG;
 	cv::Mat out_img;
+	cv::Mat pcl_img;
 
 	// Counter
 	int counts;
@@ -79,9 +81,9 @@ private:
 	ros::Publisher  pub_cloud;
 	ros::Publisher  pub_image;
 
-	message_filters::Subscriber<sensor_msgs::Image> img_sub;
+	message_filters::Subscriber<sensor_msgs::PointCloud2> pcl_sub;
 	message_filters::Subscriber<sensor_msgs::Image> depth_sub;
-	typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image> MySyncPolicy;
+	typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::PointCloud2, sensor_msgs::Image> MySyncPolicy;
 	typedef message_filters::Synchronizer<MySyncPolicy> Sync;
 	boost::shared_ptr<Sync> sync_;
 
@@ -112,7 +114,7 @@ private:
 public:
 	IMAGE_PROCESS(ros::NodeHandle&);
 	void cbCloud(const sensor_msgs::PointCloud2ConstPtr& cloud_msg);
-	void callback_sync(const sensor_msgs::ImageConstPtr&,const sensor_msgs::ImageConstPtr&);
+	void callback_sync(const sensor_msgs::PointCloud2ConstPtr&,const sensor_msgs::ImageConstPtr&);
 	void get_img_coordinate(float, float, float);
 	void pointcloud_to_image(const PointCloudXYZRGB::Ptr, PointCloudXYZRGB::Ptr);
 	void pcl_preprocess(PointCloudXYZRGB::Ptr);
@@ -120,43 +122,20 @@ public:
 
 IMAGE_PROCESS::IMAGE_PROCESS(ros::NodeHandle &n){
 	nh = n;
-	img_sub.subscribe(nh, "/X1/rgbd_camera/rgb/image_raw", 1);
+	pcl_sub.subscribe(nh, "/X1/points", 1);
 	depth_sub.subscribe(nh, "/X1/rgbd_camera/depth/image_raw", 1);
-	sync_.reset(new Sync(MySyncPolicy(10), img_sub, depth_sub));
+	sync_.reset(new Sync(MySyncPolicy(10), pcl_sub, depth_sub));
 	sync_->registerCallback(boost::bind(&IMAGE_PROCESS::callback_sync, this, _1, _2));
 	node_name = ros::this_node::getName();
 	out_img = cv::Mat(480, 640, CV_32FC1, cv::Scalar(0, 0, 0));
+	pcl_img = cv::Mat(480, 640, CV_32FC1, cv::Scalar(0, 0, 0));
 
 	// Publisher
 	pub_cloud = nh.advertise<sensor_msgs::PointCloud2> ("/ppp", 1);
 	pub_image = nh.advertise<sensor_msgs::Image> ("/dp_img", 1);
 }
 
-void IMAGE_PROCESS::callback_sync(const sensor_msgs::ImageConstPtr& image, const sensor_msgs::ImageConstPtr& depth_image){
-	cv_bridge::CvImagePtr img_ptr_img = cv_bridge::toCvCopy(image, sensor_msgs::image_encodings::RGB8);
-	cv_bridge::CvImagePtr img_ptr_depth = cv_bridge::toCvCopy(depth_image, sensor_msgs::image_encodings::TYPE_32FC1);
-	out_img = cv::Mat(480, 640, CV_32FC1, cv::Scalar(0, 0, 0));
-	for( int nrow = 0; nrow < img_ptr_depth->image.rows; nrow++){
-		for(int ncol = 0; ncol < img_ptr_depth->image.cols; ncol++){ 
-			if (std::isnan(img_ptr_depth->image.at<float>(nrow, ncol))){
-			//if (std::isnan(img_ptr_depth->image.at<unsigned short int>(nrow, ncol))){
-				out_img.at<float>(nrow,ncol) = (1.0);
-			}
-		}
-	}
-	cvIMG.header = image->header;
-	cvIMG.encoding = sensor_msgs::image_encodings::TYPE_32FC1;
-	cvIMG.image = out_img;
-	pub_image.publish(cvIMG.toImageMsg());
-}
-
-void IMAGE_PROCESS::get_img_coordinate(float x, float y,float z){
-	img_x = (x * fx)/z + cx;
-	img_y = (y * fy)/z + cy;
-	return;
-}
-
-void IMAGE_PROCESS::cbCloud(const sensor_msgs::PointCloud2ConstPtr& cloud_msg){
+void IMAGE_PROCESS::callback_sync(const sensor_msgs::PointCloud2ConstPtr& cloud_msg, const sensor_msgs::ImageConstPtr& depth_image){
 	if(!get_tf){
 		try{
 			lr.lookupTransform("/X1/rgbd_camera_link", "/X1/front_laser",
@@ -169,57 +148,59 @@ void IMAGE_PROCESS::cbCloud(const sensor_msgs::PointCloud2ConstPtr& cloud_msg){
 		}
 		return;
 	}
-
-	frame_id = cloud_msg->header.frame_id;
-	counts++;
-	//return if no cloud data
-	if ((cloud_msg->width * cloud_msg->height) == 0 || counts % 3 == 0)
-	{
-		counts = 0;
- 		return ;
-	}
-	const clock_t t_start = clock();
-
-	// transfer ros msg to point cloud
-	PointCloudXYZ::Ptr cloud_XYZ(new PointCloudXYZ);
-	PointCloudXYZRGB::Ptr cloud_XYZRGB(new PointCloudXYZRGB);
+	PointCloudXYZ::Ptr cloud_XYZ(new PointCloudXYZ);	
 	PointCloudXYZRGB::Ptr cloud_lidar(new PointCloudXYZRGB);
 	PointCloudXYZRGB::Ptr cloud_in(new PointCloudXYZRGB);
 	PointCloudXYZRGB::Ptr cloud_out(new PointCloudXYZRGB);
-	if(is_LIDAR){
-		pcl::fromROSMsg (*cloud_msg, *cloud_XYZ);
-		copyPointCloud(*cloud_XYZ, *cloud_lidar);
-		//std::cout<<tf_lidar2cam.getOrigin().x()<<"," <<tf_lidar2cam.getOrigin().y()<<","<<tf_lidar2cam.getOrigin().z()<<std::endl;
-		pcl_ros::transformPointCloud(*cloud_lidar, *cloud_in, tf_lidar2cam);
-	}
-	else{
-		pcl::fromROSMsg (*cloud_msg, *cloud_XYZRGB);
-		copyPointCloud(*cloud_XYZRGB, *cloud_in);
-	}
+
+	pcl::fromROSMsg (*cloud_msg, *cloud_XYZ);
+	copyPointCloud(*cloud_XYZ, *cloud_lidar);
+	pcl_ros::transformPointCloud(*cloud_lidar, *cloud_in, tf_lidar2cam);
 	copyPointCloud(*cloud_in, *cloud_out);
+	pcl_img = cv::Mat(480, 640, CV_32FC1, cv::Scalar(0, 0, 0));
 
-	// Remove out of range points and robot points
-	//pcl_preprocess(cloud_out);
-	
-	//cv::Mat depth_image(480, 640, CV_32FC1, cv::Scalar(0, 0, 0));
-	pointcloud_to_image(cloud_in, cloud_out);
-	clock_t t_end = clock();
-	//cout << "PointCloud preprocess time taken = " << (t_end-t_start)/(double)(CLOCKS_PER_SEC) << endl;
+	for(int i = 0; i < cloud_in->points.size(); i++){
+		float x;
+		float y;
+		x = float(cloud_in->points[i].y);
+		y = float(cloud_in->points[i].z);
+		img_z = float(cloud_in->points[i].x);
+		if(img_z < 0){
+			continue;
+		}
+		get_img_coordinate(x, y, img_z);
+		if (int(img_x) < 640 && int(img_y) < 480 && int(img_x) >= 0 && int(img_y) >= 0){
+			cloud_out->points[i].r = 255;
+			cloud_out->points[i].g = 255;
+			cloud_out->points[i].b = 0;
+			pcl_img.at<float>(480-int(img_y), 640-int(img_x)) = 255;
+		}
+	}
 
-	// PUblish image
-	//std::cout << result.cols << ',' << result.rows << std::endl;
-	cvIMG.header = cloud_msg->header;
+	//cv_bridge::CvImagePtr img_ptr_img = cv_bridge::toCvCopy(image, sensor_msgs::image_encodings::RGB8);
+	cv_bridge::CvImagePtr img_ptr_depth = cv_bridge::toCvCopy(depth_image, sensor_msgs::image_encodings::TYPE_32FC1);
+	out_img = cv::Mat(480, 640, CV_32FC1, cv::Scalar(0, 0, 0));
+	for( int nrow = 0; nrow < img_ptr_depth->image.rows; nrow++){
+		for(int ncol = 0; ncol < img_ptr_depth->image.cols; ncol++){ 
+			if (std::isnan(img_ptr_depth->image.at<float>(nrow, ncol))){
+			//if (std::isnan(img_ptr_depth->image.at<unsigned short int>(nrow, ncol))){
+				out_img.at<float>(nrow,ncol) = (1.0);
+				pcl_img.at<float>(nrow,ncol) = 0.0;
+			}
+		}
+	}
+	cvIMG.header = depth_image->header;
 	cvIMG.encoding = sensor_msgs::image_encodings::TYPE_32FC1;
-	cvIMG.image = out_img;
-	//img_msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", depth_image).toImageMsg();
+	cvIMG.image = pcl_img;
+	cv::imwrite("pcl_img.png", pcl_img);
+	cv::imwrite("depth_img.png", img_ptr_depth->image);
 	pub_image.publish(cvIMG.toImageMsg());
+}
 
-	// Publish point cloud
-	sensor_msgs::PointCloud2 pcl_output;
-	pcl::toROSMsg(*cloud_out, pcl_output);
-	pcl_output.header = cloud_msg->header;
-	pcl_output.header.frame_id = "/X1/rgbd_camera_link";
-	pub_cloud.publish(pcl_output);
+void IMAGE_PROCESS::get_img_coordinate(float x, float y,float z){
+	img_x = (x * fx)/z + cx;
+	img_y = (y * fy)/z + cy;
+	return;
 }
 
 void IMAGE_PROCESS::pointcloud_to_image(const PointCloudXYZRGB::Ptr cloud_in, PointCloudXYZRGB::Ptr cloud_out){
